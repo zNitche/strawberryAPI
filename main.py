26fcbb7b370014f14535b5dace53fdeaa0caec89
zNitche <53448802+zNitche@users.noreply.github.com>
2023-01-31 21:43:32 +0100

    added onboard led state when servers running

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from eagle.controllers.server import Server
 from eagle.controllers.app import App
 from configs.server_config import ServerConfig
+from eagle.utils import machine_utils
 
 
 def main():
@@ -8,6 +9,9 @@ def main():
     app = App()
 
     server.run_as_host() if ServerConfig.HOTSPOT_MODE else server.run_as_client()
+
+    if server.wlan is not None:
+        machine_utils.get_onboard_led().on()
 
     server.set_app(app)
     server.run_mainloop()
