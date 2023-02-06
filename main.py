8f0899b1d02c9f0f0688a0bc11e42530589733d8
zNitche <53448802+zNitche@users.noreply.github.com>
2023-02-06 11:33:31 +0100

    fixed config errors

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from eagle.controllers.server import Server
 from eagle.controllers.app import App
-from configs.server_config import ServerConfig
+from config import ServerConfig
 from eagle.utils import machine_utils
 
 
 def main():
     server = Server()
-    app = App()
 
     server.run_as_host() if ServerConfig.HOTSPOT_MODE else server.run_as_client()
 
     if server.wlan is not None:
         machine_utils.get_onboard_led().on()
 
-    server.set_app(app)
-    server.run_mainloop()
+        server.set_app(App())
+        server.run_mainloop()
 
 
 if __name__ == '__main__':
