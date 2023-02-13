02ace90113a0f5c326b29edd7e0e56d4de97afa6
zNitche <53448802+zNitche@users.noreply.github.com>
2023-02-13 21:31:35 +0100

    improved templates inheritance (for many sections)

--- strawberry/parsers/template_parser.py
+++ strawberry/parsers/template_parser.py
@@ -1,16 +1,19 @@
 from strawberry.utils import files_utils
 import sys
+import re
 
 
 class TemplateParser:
     def __init__(self):
-        self.base_template_define_start = "#EXTEND"
+        self.base_template_define = "#EXTEND"
 
         self.var_start = "{{"
         self.var_end = "}}"
 
         self.template_section_start = "#SECTION"
         self.template_section_end = "#END"
+
+        self.template_section_slot = "#SECTION_SLOT"
 
     def parse_template(self, template_content, context):
         try:
@@ -33,42 +36,40 @@ class TemplateParser:
     def merge_with_parent_template(self, template):
         first_row = template.split("\n")[0].strip()
 
-        if first_row.startswith(self.base_template_define_start):
+        if first_row.startswith(self.base_template_define):
             base_template_path = first_row.split()[-1]
             base_template_content = files_utils.get_file_content(base_template_path)
 
             if base_template_content:
-                template = template.replace(f"{self.base_template_define_start} {base_template_path}", "")
+                template = template.replace(f"{self.base_template_define} {base_template_path}", "")
 
-                base_template_sections = self.get_sections_from_template(base_template_content)
+                base_template_sections_slots = self.get_sections_slots_from_template(base_template_content)
                 template_sections = self.get_sections_from_template(template)
 
                 template = self.process_sections(template,
                                                  base_template_content,
                                                  template_sections,
-                                                 base_template_sections)
+                                                 base_template_sections_slots)
 
         return template
 
-    def process_sections(self, template, base_template, template_sections, base_sections):
-        splitted_base_template = base_template.split("\n")
+    def process_sections(self, template, base_template, template_sections, sections_slots_names):
         splitted_template = template.split("\n")
 
         for template_section_name in template_sections:
             template_section = template_sections.get(template_section_name)
-            base_section = base_sections.get(template_section_name)
 
-            if base_section:
-                section_start = base_section["start"]
-                section_end = base_section["end"] + 1
+            if template_section_name in sections_slots_names:
+                base_section_slot_name = f"{self.template_section_slot} {template_section_name}"
+                templ_between = splitted_template[template_section["start"]: template_section["end"] + 1]
 
-                templ_between = splitted_template[template_section["start"]: template_section["end"]]
+                base_template = re.sub(f"{base_section_slot_name}\n", "\n".join(templ_between), base_template)
 
-                splitted_base_template = splitted_base_template[:section_start - 1] + \
-                                         templ_between + \
-                                         splitted_base_template[section_end + 1:]
+                sections_slots_names.remove(template_section_name)
 
-        return "\n".join(splitted_base_template)
+        base_template = self.clear_section_slots(base_template, sections_slots_names)
+
+        return base_template
 
     def get_sections_from_template(self, template):
         sections = {}
@@ -92,3 +93,20 @@ class TemplateParser:
                     }
 
         return sections
+
+    def get_sections_slots_from_template(self, template):
+        sections_slots = []
+
+        for row_id, row in enumerate(template.split("\n")):
+            stripped_row = row.strip()
+
+            if stripped_row.startswith(self.template_section_start):
+                sections_slots.append(stripped_row.split()[-1])
+
+        return sections_slots
+
+    def clear_section_slots(self, template, slots_names):
+        for name in slots_names:
+            template = template.replace(f"{self.template_section_slot} {name}", "")
+
+        return template
