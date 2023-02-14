c98cc800091b1514b851d99857e549e597831aa7
zNitche <53448802+zNitche@users.noreply.github.com>
2023-02-14 17:44:03 +0100

    improved urls matching speed

--- strawberry/routes/route.py
+++ strawberry/routes/route.py
@@ -8,9 +8,10 @@ class Route(RouteBase):
         self.url = url
         self.methods = methods if methods else ["GET"]
 
+        self.path_argument_start = "<"
+        self.path_argument_end = ">"
+
     def get_url_pattern_data(self):
-        path_argument_start = "<"
-        path_argument_end = ">"
         splitted_url = self.url.split("/")
 
         if splitted_url[0] == "":
@@ -19,8 +20,8 @@ class Route(RouteBase):
         url_pattern_data = []
 
         for url_part in splitted_url:
-            is_parameter = True if url_part.startswith(path_argument_start) and \
-                                   url_part.endswith(path_argument_end) else False
+            is_parameter = True if url_part.startswith(self.path_argument_start) and \
+                                   url_part.endswith(self.path_argument_end) else False
 
             url_pattern_data.append({
                 "url_part": url_part,
@@ -29,7 +30,10 @@ class Route(RouteBase):
 
         return url_pattern_data
 
-    def match_url(self, url):
+    def match_bare_url(self, url):
+        return True if url == self.url else False
+
+    def match_url_with_parameters(self, url):
         splitted_url = url.split("/")
         splitted_self_url = self.url.split("/")
 
@@ -53,3 +57,9 @@ class Route(RouteBase):
 
         return True if (lengths_match and pattern_match and non_empty_parts) else False
 
+    def match_url(self, url):
+        if self.path_argument_start in self.url and self.path_argument_end in self.url:
+            return self.match_url_with_parameters(url)
+
+        else:
+            return self.match_bare_url(url)
