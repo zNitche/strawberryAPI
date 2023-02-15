041c2492331581cc1abf221359c4910abaa901db
zNitche <53448802+zNitche@users.noreply.github.com>
2023-02-15 15:48:03 +0100

    tweaked app mainloop errors handling

--- strawberry/controllers/app.py
+++ strawberry/controllers/app.py
@@ -1,6 +1,7 @@
 from strawberry.communication.response import Response
 from strawberry.communication.file_response import FileResponse
 from strawberry.routes.default.errors import errors
+import sys
 
 
 class App:
@@ -18,23 +19,31 @@ class App:
     async def requests_handler(self, client_addr, request):
         response = self.raise_error(500)
 
-        if request:
-            self.print_debug(f"request header from {client_addr} :{request.header}")
-            self.print_debug(f"request body from {client_addr} :{request.body} | {type(request.body)}")
+        try:
+            if request:
+                self.print_debug(f"request header from {client_addr} :{request.header}")
+                self.print_debug(f"request body from {client_addr} :{request.body} | {type(request.body)}")
 
-            if self.__check_if_requested_static_file(request):
-                self.print_debug("requested static file")
+                if self.__check_if_requested_static_file(request):
+                    self.print_debug("requested static file")
 
-                target = request.target.split("static/")[-1]
-                response = self.__get_static_file(target)
+                    target = request.target.split("static/")[-1]
+                    response = self.__get_static_file(target)
 
-            else:
-                self.print_debug("requested route")
-                response = self.__process_route(request)
+                else:
+                    self.print_debug("requested route")
+                    response = self.__process_route(request)
 
-            self.print_debug(f"response string for {client_addr}: '{response.get_response_string()}'")
+                self.print_debug(f"response string for {client_addr}: '{response.get_response_string()}'")
 
-        return response.get_response_string()
+        except Exception as e:
+            self.print_debug(f"error occurred: {str(e)}")
+
+            if self.debug_mode:
+                sys.print_exception(e)
+
+        finally:
+            return response.get_response_string()
 
     def __get_route_for_url(self, url):
         target_route = None
