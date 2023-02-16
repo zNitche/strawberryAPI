7d2862786598902fc668201c7691057e3c35921e
zNitche <53448802+zNitche@users.noreply.github.com>
2023-02-16 22:31:02 +0100

    route.py cleanup

--- strawberry/routes/route.py
+++ strawberry/routes/route.py
@@ -6,6 +6,7 @@ class Route(RouteBase):
         super().__init__(handler)
 
         self.url = url
+        self.splitted_url = self.split_url(url)
         self.methods = methods if methods else ["GET"]
 
         self.path_argument_start = "<"
@@ -18,6 +19,35 @@ class Route(RouteBase):
             splitted_url.pop(0)
 
         return splitted_url
+
+    def accepts_path_parameters(self):
+        return self.path_argument_start in self.url and self.path_argument_end in self.url
+
+    def match_url(self, url):
+        match = self.match_url_with_parameters(url) if self.accepts_path_parameters() else self.match_bare_url(url)
+
+        return match
+
+    def match_bare_url(self, url):
+        return True if url == self.url else False
+
+    def match_url_with_parameters(self, url):
+        splitted_url = self.split_url(url)
+        url_pattern_data = self.get_url_pattern_data()
+
+        lengths_match = True if len(splitted_url) == len(self.splitted_url) else False
+        non_empty_parts = True if (("" not in splitted_url) and (" " not in splitted_url)) else False
+        pattern_match = True
+
+        if lengths_match:
+            for part_id, url_pattern in enumerate(url_pattern_data):
+                splitted_url_part = splitted_url[part_id]
+
+                if (splitted_url_part != url_pattern["url_part"]) and not url_pattern["is_parameter"]:
+                    pattern_match = False
+                    break
+
+        return True if (lengths_match and pattern_match and non_empty_parts) else False
 
     def concat_url_with_parameters(self, parameters):
         url = self.url
@@ -33,62 +63,30 @@ class Route(RouteBase):
         return url
 
     def get_url_pattern_data(self):
-        splitted_url = self.split_url(self.url)
         url_pattern_data = []
 
-        for url_position, url_part in enumerate(splitted_url):
-            is_parameter = True if url_part.startswith(self.path_argument_start) and \
-                                   url_part.endswith(self.path_argument_end) else False
+        for url_position, url_part in enumerate(self.splitted_url):
+            is_parameter = True if self.is_parameter(url_part) else False
 
             url_pattern_data.append({
                 "url_position": url_position,
                 "url_part": url_part,
-                "is_parameter": is_parameter
+                "is_parameter": is_parameter,
+                "parameter_name": self.get_path_parameter_name_by_position(url_position)
             })
 
         return url_pattern_data
 
     def get_path_parameter_name_by_position(self, url_position):
-        splitted_url = self.split_url(self.url)
         parameter_name = None
 
-        if url_position < len(splitted_url):
-            url_item = splitted_url[url_position]
+        if url_position < len(self.splitted_url):
+            url_item = self.splitted_url[url_position]
 
-            parameter_name = url_item.replace(self.path_argument_start, "").replace(self.path_argument_end, "")
+            if self.is_parameter(url_item):
+                parameter_name = url_item[1:-1]
 
         return parameter_name
-
-    def match_bare_url(self, url):
-        return True if url == self.url else False
-
-    def match_url_with_parameters(self, url):
-        splitted_url = self.split_url(url)
-        splitted_self_url = self.split_url(self.url)
-
-        url_pattern_data = self.get_url_pattern_data()
-
-        lengths_match = True if len(splitted_url) == len(splitted_self_url) else False
-        non_empty_parts = True if (("" not in splitted_url) and (" " not in splitted_url)) else False
-        pattern_match = True
-
-        if lengths_match:
-            for part_id, url_pattern in enumerate(url_pattern_data):
-                splitted_url_part = splitted_url[part_id]
-
-                if (splitted_url_part != url_pattern["url_part"]) and not url_pattern["is_parameter"]:
-                    pattern_match = False
-                    break
-
-        return True if (lengths_match and pattern_match and non_empty_parts) else False
-
-    def accepts_path_parameters(self):
-        return self.path_argument_start in self.url and self.path_argument_end in self.url
-
-    def match_url(self, url):
-        match = self.match_url_with_parameters(url) if self.accepts_path_parameters() else self.match_bare_url(url)
-
-        return match
 
     def get_path_parameters_for_url(self, url):
         parameters = {}
@@ -100,9 +98,12 @@ class Route(RouteBase):
             for data_part in url_pattern_data:
                 if data_part["is_parameter"]:
                     url_position = data_part["url_position"]
-                    parameter_name = self.get_path_parameter_name_by_position(url_position)
+                    parameter_name = data_part["parameter_name"]
 
                     if parameter_name and url_position < len(splitted_url):
                         parameters[parameter_name] = splitted_url[url_position]
 
         return parameters
+
+    def is_parameter(self, name):
+        return True if name.startswith(self.path_argument_start) and name.endswith(self.path_argument_end) else False
