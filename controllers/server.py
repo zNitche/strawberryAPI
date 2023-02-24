a36a29dcd77fe4f943ba0c04d28029f06a9b3f5a
zNitche <53448802+zNitche@users.noreply.github.com>
2023-02-24 20:55:40 +0100

    master sync 011995d935a68d80763759d11a006085d4dca5fd

--- controllers/server.py
+++ controllers/server.py
@@ -5,7 +5,7 @@ import uasyncio
 import gc
 from strawberry.utils import machine_utils
 from strawberry.communication.request import Request
-from strawberry.consts import HTTPConsts
+from strawberry.consts import HTTPConsts, ServerConsts
 
 
 class Server:
@@ -15,6 +15,7 @@ class Server:
                  wifi_ssid="",
                  wifi_password="",
                  wifi_connections_retries=5,
+                 wifi_connection_retries_till_connected=False,
                  wifi_connection_delay=5,
                  hotspot_name="pico_hotspot",
                  hotspot_password="pico_hotspot1234",
@@ -22,20 +23,28 @@ class Server:
 
         self.host = host
         self.port = port
+
         self.wifi_ssid = wifi_ssid
         self.wifi_password = wifi_password
+
         self.wifi_connections_retries = wifi_connections_retries
+        self.wifi_connection_retries_till_connected = wifi_connection_retries_till_connected
         self.wifi_connection_delay = wifi_connection_delay
+
         self.hotspot_name = hotspot_name
         self.hotspot_password = hotspot_password
         self.hotspot_mode = hotspot_mode
 
         self.wlan = None
-
         self.app = None
+
+        self.onboard_led = machine_utils.get_onboard_led()
 
         self.debug_mode = debug_mode
         self.mainloop = uasyncio.get_event_loop()
+
+        self.led_timer = machine_utils.create_timer()
+        self.wifi_reconnect_timer = machine_utils.create_timer() if not self.hotspot_mode else None
 
     def set_app(self, app):
         self.app = app
@@ -44,7 +53,6 @@ class Server:
         self.print_debug(f"setting up server as client...")
 
         self.wlan = network.WLAN(network.STA_IF)
-
         self.wlan.active(True)
 
     def __setup_wlan_as_host(self):
@@ -56,17 +64,24 @@ class Server:
         self.wlan.active(True)
 
         self.print_debug(f"WLAN config: {self.wlan.ifconfig()}")
+        machine_utils.init_periodic_timer(self.led_timer,
+                                          ServerConsts.LED_BLINK_WIFI_CONNECTED,
+                                          self.onboard_led.toggle)
 
     def __connect_to_network(self):
         tries = 0
 
         self.wlan.disconnect()
 
-        while (tries < self.wifi_connections_retries) and (not self.wlan.isconnected()):
+        while ((tries < self.wifi_connections_retries) or self.wifi_connection_retries_till_connected)\
+                and (not self.wlan.isconnected()):
+
             self.print_debug(f"connecting to network: {tries}...")
 
             self.wlan.connect(self.wifi_ssid, self.wifi_password)
-            machine_utils.blink_onboard_led()
+            machine_utils.init_periodic_timer(self.led_timer,
+                                              ServerConsts.LED_BLINK_PERIOD_WIFI_CONNECTING,
+                                              self.onboard_led.toggle)
 
             time.sleep(self.wifi_connection_delay)
             tries += 1
@@ -75,7 +90,15 @@ class Server:
             self.print_debug(f"connected to '{self.wifi_ssid}'")
             self.print_debug(f"WLAN config: {self.wlan.ifconfig()}")
 
-            machine_utils.blink_onboard_led(blinks=3)
+            machine_utils.init_periodic_timer(self.led_timer,
+                                              ServerConsts.LED_BLINK_WIFI_CONNECTED,
+                                              self.onboard_led.toggle)
+
+    def __reconnect_to_network(self):
+        if not self.wlan.isconnected() and not self.hotspot_mode:
+            self.print_debug(f"reconnecting to '{self.wifi_ssid}'")
+
+            self.__connect_to_network()
 
     def run(self):
         self.__run_as_host() if self.hotspot_mode else self.__run_as_client()
@@ -160,6 +183,11 @@ class Server:
 
         if self.wlan is not None:
             self.mainloop.create_task(uasyncio.start_server(self.__requests_handler, self.host, self.port))
+
+            if self.wifi_reconnect_timer:
+                machine_utils.init_periodic_timer(self.wifi_reconnect_timer,
+                                                  ServerConsts.WIFI_RECONNECT_PERIOD,
+                                                  self.__reconnect_to_network)
 
             self.print_debug("mainloop running...")
             self.mainloop.run_forever()
